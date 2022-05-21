6cf98c98401502add45ad4e3d22fdbd34b58f88f
Jake O <56329025+jakeorchanian@users.noreply.github.com>
2022-05-21 05:39:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function mouseOverWB() {
+    document.getElementById("watermelonBunny").src = "no-seed-watermelon-bunny.png";
+}
+
+function mouseOutWB() {
+    document.getElementById("watermelonBunny").src = "watermelon-bunny.png";
+}
+
+function mouseOverSB() {
+    document.getElementById("santaBunny").src = "christmas-bunny.png";
+}
+
+function mouseOutSB() {
+    document.getElementById("santaBunny").src = "santa-bunny.png";
+}
+
+function mouseOverDB() {
+    document.getElementById("dreamBunny").src = "white-dream-bunny.png";
+}
+
+function mouseOutDB() {
+    document.getElementById("dreamBunny").src = "dream-bunny.png";
+}
+
+function mouseOverBB() {
+    document.getElementById("blurpleBunny").src = "old-blurple-bunny.png";
+}
+
+function mouseOutBB() {
+    document.getElementById("blurpleBunny").src = "blurple-bunny.png";
+}
+
+function mouseOverNB() {
+    document.getElementById("newYearsBunny").src = "new-years-bunny-2022.png";
+}
+
+function mouseOutNB() {
+    document.getElementById("newYearsBunny").src = "new-years-bunny.png";
+}
+
+function mouseOverLB() {
+    document.getElementById("leprechaunBunny").src = "leprechaun-bunny-no-hat.png";
+}
+
+function mouseOutLB() {
+    document.getElementById("leprechaunBunny").src = "leprechaun-bunny.png";
+}
+
+function mouseOverEB() {
+    document.getElementById("easterBunny").src = "easter-bunny-no-eggs.png";
+}
+
+function mouseOutEB() {
+    document.getElementById("easterBunny").src = "easter-bunny.png";
+}
+
+function mouseOverFB() {
+    document.getElementById("flowerBunny").src = "flower-bunny-no-flowers.png";
+}
+
+function mouseOutFB() {
+    document.getElementById("flowerBunny").src = "flower-bunny.png";
+}
