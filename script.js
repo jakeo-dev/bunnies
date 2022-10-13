8908cdb60e0b794c715c2641f1fa60e62021f39f
Jake O <56329025+jakeorch@users.noreply.github.com>
2022-10-12 18:33:29 -0700

    Actually fixed collection divs (probably)

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function mouseOverDB() {
-    document.getElementById("dreamBunny").src = "images/white-dream-bunny.png";
+    document.getElementById('dreamBunny').src = 'images/white-dream-bunny.png';
 }
 
 function mouseOutDB() {
-    document.getElementById("dreamBunny").src = "images/dream-bunny.png";
+    document.getElementById('dreamBunny').src = 'images/dream-bunny.png';
 }
 
 function mouseOverBB() {
-    document.getElementById("blurpleBunny").src = "images/old-blurple-bunny.png";
+    document.getElementById('blurpleBunny').src = 'images/old-blurple-bunny.png';
 }
 
 function mouseOutBB() {
-    document.getElementById("blurpleBunny").src = "images/blurple-bunny.png";
+    document.getElementById('blurpleBunny').src = 'images/blurple-bunny.png';
 }
 
 function mouseOverNB() {
-    document.getElementById("newYearsBunny").src = "images/new-years-bunny-2022.png";
+    document.getElementById('newYearsBunny').src = 'images/new-years-bunny-2022.png';
 }
 
 function mouseOutNB() {
-    document.getElementById("newYearsBunny").src = "images/new-years-bunny.png";
+    document.getElementById('newYearsBunny').src = 'images/new-years-bunny.png';
 }
 
 function mouseOverLB() {
-    document.getElementById("leprechaunBunny").src = "images/leprechaun-bunny-no-hat.png";
+    document.getElementById('leprechaunBunny').src = 'images/leprechaun-bunny-no-hat.png';
 }
 
 function mouseOutLB() {
-    document.getElementById("leprechaunBunny").src = "images/leprechaun-bunny.png";
+    document.getElementById('leprechaunBunny').src = 'images/leprechaun-bunny.png';
 }
 
 function mouseOverEB() {
-    document.getElementById("easterBunny").src = "images/easter-bunny.png";
+    document.getElementById('easterBunny').src = 'images/easter-bunny.png';
 }
 
 function mouseOutEB() {
-    document.getElementById("easterBunny").src = "images/easter-bunny-no-eggs.png";
+    document.getElementById('easterBunny').src = 'images/easter-bunny-no-eggs.png';
 }
