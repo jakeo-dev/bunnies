8ce6abd4a40f9bdcb37012223f4578452c507746
Jake O <56329025+jakeorch@users.noreply.github.com>
2022-10-12 18:12:52 -0700

    Updated for images folder again

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-function mouseOverWB() {
-    document.getElementById("watermelonBunny").src = "no-seed-watermelon-bunny.png";
-}
-
-function mouseOutWB() {
-    document.getElementById("watermelonBunny").src = "watermelon-bunny.png";
-}
-
 function mouseOverDB() {
-    document.getElementById("dreamBunny").src = "white-dream-bunny.png";
+    document.getElementById("dreamBunny").src = "images/white-dream-bunny.png";
 }
 
 function mouseOutDB() {
-    document.getElementById("dreamBunny").src = "dream-bunny.png";
+    document.getElementById("dreamBunny").src = "images/dream-bunny.png";
 }
 
 function mouseOverBB() {
-    document.getElementById("blurpleBunny").src = "old-blurple-bunny.png";
+    document.getElementById("blurpleBunny").src = "images/old-blurple-bunny.png";
 }
 
 function mouseOutBB() {
-    document.getElementById("blurpleBunny").src = "blurple-bunny.png";
+    document.getElementById("blurpleBunny").src = "images/blurple-bunny.png";
 }
 
 function mouseOverNB() {
-    document.getElementById("newYearsBunny").src = "new-years-bunny-2022.png";
+    document.getElementById("newYearsBunny").src = "images/new-years-bunny-2022.png";
 }
 
 function mouseOutNB() {
-    document.getElementById("newYearsBunny").src = "new-years-bunny.png";
+    document.getElementById("newYearsBunny").src = "images/new-years-bunny.png";
 }
 
 function mouseOverLB() {
-    document.getElementById("leprechaunBunny").src = "leprechaun-bunny-no-hat.png";
+    document.getElementById("leprechaunBunny").src = "images/leprechaun-bunny-no-hat.png";
 }
 
 function mouseOutLB() {
-    document.getElementById("leprechaunBunny").src = "leprechaun-bunny.png";
+    document.getElementById("leprechaunBunny").src = "images/leprechaun-bunny.png";
 }
 
 function mouseOverEB() {
-    document.getElementById("easterBunny").src = "easter-bunny.png";
+    document.getElementById("easterBunny").src = "images/easter-bunny.png";
 }
 
 function mouseOutEB() {
-    document.getElementById("easterBunny").src = "easter-bunny-no-eggs.png";
+    document.getElementById("easterBunny").src = "images/easter-bunny-no-eggs.png";
 }
